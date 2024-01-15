757019d95708f1515ce8dc8bd8e6b85f81d617c6
Tim <cassell.timothy@gmail.com>
2024-01-14 22:51:22 -0500

    Added `AsyncEnumerable.AnyAsync` extensions.

---
+++ Package/Core/Linq/Operators/AnyAsync.cs
@@ -0,0 +1,250 @@
+#if PROTO_PROMISE_DEBUG_ENABLE || (!PROTO_PROMISE_DEBUG_DISABLE && DEBUG)
+#define PROMISE_DEBUG
+#else
+#undef PROMISE_DEBUG
+#endif
+
+using Proto.Promises.Async.CompilerServices;
+using System;
+
+namespace Proto.Promises.Linq
+{
+#if CSHARP_7_3_OR_NEWER
+    partial class AsyncEnumerable
+    {
+        /// <summary>
+        /// Determines whether an async-enumerable sequence contains any elements.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">Source sequence to check for emptiness.</param>
+        /// <param name="cancelationToken">The optional cancelation token to be used for canceling the sequence at any time.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether the <paramref name="source"/> contains any elements.</returns>
+        public static Promise<bool> AnyAsync<TSource>(this AsyncEnumerable<TSource> source, CancelationToken cancelationToken = default)
+            => AnyCore(source.GetAsyncEnumerator(cancelationToken));
+
+        private static async Promise<bool> AnyCore<TSource>(AsyncEnumerator<TSource> asyncEnumerator)
+        {
+            try
+            {
+                return await asyncEnumerator.MoveNextAsync();
+            }
+            finally
+            {
+                await asyncEnumerator.DisposeAsync();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether any element of an async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="cancelationToken">The optional cancelation token to be used for canceling the sequence at any time.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="source"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource>(this AsyncEnumerable<TSource> source, Func<TSource, bool> predicate, CancelationToken cancelationToken = default)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreSync(source.GetAsyncEnumerator(cancelationToken), Internal.PromiseRefBase.DelegateWrapper.Create(predicate));
+        }
+
+        /// <summary>
+        /// Determines whether any element of an async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <typeparam name="TCapture">The type of the captured value.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <param name="captureValue">The extra value that will be passed to <paramref name="predicate"/>.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="cancelationToken">The optional cancelation token to be used for canceling the sequence at any time.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="source"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource, TCapture>(this AsyncEnumerable<TSource> source, TCapture captureValue, Func<TCapture, TSource, bool> predicate, CancelationToken cancelationToken = default)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreSync(source.GetAsyncEnumerator(cancelationToken), Internal.PromiseRefBase.DelegateWrapper.Create(captureValue, predicate));
+        }
+
+        private static async Promise<bool> AnyCoreSync<TSource, TPredicate>(AsyncEnumerator<TSource> asyncEnumerator, TPredicate predicate)
+            where TPredicate : Internal.IFunc<TSource, bool>
+        {
+            try
+            {
+                while (await asyncEnumerator.MoveNextAsync())
+                {
+                    if (predicate.Invoke(asyncEnumerator.Current))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            finally
+            {
+                await asyncEnumerator.DisposeAsync();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether any element of an async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <param name="predicate">An async function to test each element for a condition.</param>
+        /// <param name="cancelationToken">The optional cancelation token to be used for canceling the sequence at any time.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="source"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource>(this AsyncEnumerable<TSource> source, Func<TSource, Promise<bool>> predicate, CancelationToken cancelationToken = default)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreAsync(source.GetAsyncEnumerator(cancelationToken), Internal.PromiseRefBase.DelegateWrapper.Create(predicate));
+        }
+
+        /// <summary>
+        /// Determines whether any element of an async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <typeparam name="TCapture">The type of the captured value.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <param name="captureValue">The extra value that will be passed to <paramref name="predicate"/>.</param>
+        /// <param name="predicate">An async function to test each element for a condition.</param>
+        /// <param name="cancelationToken">The optional cancelation token to be used for canceling the sequence at any time.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="source"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource, TCapture>(this AsyncEnumerable<TSource> source, TCapture captureValue, Func<TCapture, TSource, Promise<bool>> predicate, CancelationToken cancelationToken = default)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreAsync(source.GetAsyncEnumerator(cancelationToken), Internal.PromiseRefBase.DelegateWrapper.Create(captureValue, predicate));
+        }
+
+        private static async Promise<bool> AnyCoreAsync<TSource, TPredicate>(AsyncEnumerator<TSource> asyncEnumerator, TPredicate predicate)
+            where TPredicate : Internal.IFunc<TSource, Promise<bool>>
+        {
+            try
+            {
+                while (await asyncEnumerator.MoveNextAsync())
+                {
+                    if (await predicate.Invoke(asyncEnumerator.Current))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            finally
+            {
+                await asyncEnumerator.DisposeAsync();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether any element of a configured async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="configuredSource">Configured source sequence.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="configuredSource"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource>(this in ConfiguredAsyncEnumerable<TSource> configuredSource, Func<TSource, bool> predicate)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreSync(configuredSource.GetAsyncEnumerator(), Internal.PromiseRefBase.DelegateWrapper.Create(predicate));
+        }
+
+        /// <summary>
+        /// Determines whether any element of a configured async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <typeparam name="TCapture">The type of the captured value.</typeparam>
+        /// <param name="configuredSource">Configured source sequence.</param>
+        /// <param name="captureValue">The extra value that will be passed to <paramref name="predicate"/>.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="configuredSource"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource, TCapture>(this in ConfiguredAsyncEnumerable<TSource> configuredSource, TCapture captureValue, Func<TCapture, TSource, bool> predicate)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreSync(configuredSource.GetAsyncEnumerator(), Internal.PromiseRefBase.DelegateWrapper.Create(captureValue, predicate));
+        }
+
+        private static async Promise<bool> AnyCoreSync<TSource, TPredicate>(ConfiguredAsyncEnumerable<TSource>.Enumerator asyncEnumerator, TPredicate predicate)
+            where TPredicate : Internal.IFunc<TSource, bool>
+        {
+            try
+            {
+                while (await asyncEnumerator.MoveNextAsync())
+                {
+                    if (predicate.Invoke(asyncEnumerator.Current))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            finally
+            {
+                await asyncEnumerator.DisposeAsync();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether any element of a configured async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="configuredSource">Configured source sequence.</param>
+        /// <param name="predicate">An async function to test each element for a condition.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="configuredSource"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource>(this in ConfiguredAsyncEnumerable<TSource> configuredSource, Func<TSource, Promise<bool>> predicate)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreAsync(configuredSource.GetAsyncEnumerator(), Internal.PromiseRefBase.DelegateWrapper.Create(predicate));
+        }
+
+        /// <summary>
+        /// Determines whether any element of a configured async-enumerable sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <typeparam name="TCapture">The type of the captured value.</typeparam>
+        /// <param name="configuredSource">Configured source sequence.</param>
+        /// <param name="captureValue">The extra value that will be passed to <paramref name="predicate"/>.</param>
+        /// <param name="predicate">An async function to test each element for a condition.</param>
+        /// <returns><see cref="Promise{T}"/> containing the result of whether <paramref name="configuredSource"/> is not empty and any element in the source sequence passed the test in the specified <paramref name="predicate"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="predicate"/> is null.</exception>
+        public static Promise<bool> AnyAsync<TSource, TCapture>(this in ConfiguredAsyncEnumerable<TSource> configuredSource, TCapture captureValue, Func<TCapture, TSource, Promise<bool>> predicate)
+        {
+            ValidateArgument(predicate, nameof(predicate), 1);
+
+            return AnyCoreAsync(configuredSource.GetAsyncEnumerator(), Internal.PromiseRefBase.DelegateWrapper.Create(captureValue, predicate));
+        }
+
+        private static async Promise<bool> AnyCoreAsync<TSource, TPredicate>(ConfiguredAsyncEnumerable<TSource>.Enumerator asyncEnumerator, TPredicate predicate)
+            where TPredicate : Internal.IFunc<TSource, Promise<bool>>
+        {
+            try
+            {
+                while (await asyncEnumerator.MoveNextAsync())
+                {
+                    if (await predicate.Invoke(asyncEnumerator.Current))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            finally
+            {
+                await asyncEnumerator.DisposeAsync();
+            }
+        }
+    }
+#endif // CSHARP_7_3_OR_NEWER
+}
---
+++ Package/Tests/CoreTests/APIs/Linq/Operators/AnyAsyncTests.cs
@@ -0,0 +1,225 @@
+﻿#if CSHARP_7_3_OR_NEWER
+
+#if PROTO_PROMISE_DEBUG_ENABLE || (!PROTO_PROMISE_DEBUG_DISABLE && DEBUG)
+#define PROMISE_DEBUG
+#else
+#undef PROMISE_DEBUG
+#endif
+
+using NUnit.Framework;
+using Proto.Promises;
+using Proto.Promises.Async.CompilerServices;
+using Proto.Promises.Linq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+namespace ProtoPromiseTests.APIs.Linq
+{
+    public static class AnyHelper
+    {
+        // We test all the different overloads.
+        public static Promise<bool> AnyAsync<TSource>(this AsyncEnumerable<TSource> source,
+            bool configured,
+            bool async,
+            Func<TSource, bool> predicate, bool captureValue)
+        {
+            if (configured)
+            {
+                return AnyAsync(source.ConfigureAwait(SynchronizationOption.Foreground), async, predicate, captureValue);
+            }
+
+            const string valueCapture = "valueCapture";
+
+            if (!captureValue)
+            {
+                return async
+                    ? source.AnyAsync(async x => predicate(x))
+                    : source.AnyAsync(predicate);
+            }
+            else
+            {
+                return async
+                    ? source.AnyAsync(valueCapture, async (cv, x) =>
+                    {
+                        Assert.AreEqual(valueCapture, cv);
+                        return predicate(x);
+                    })
+                    : source.AnyAsync(valueCapture, (cv, x) =>
+                    {
+                        Assert.AreEqual(valueCapture, cv);
+                        return predicate(x);
+                    });
+            }
+        }
+
+        public static Promise<bool> AnyAsync<TSource>(this ConfiguredAsyncEnumerable<TSource> source,
+            bool async,
+            Func<TSource, bool> predicate, bool captureValue)
+        {
+            const string valueCapture = "valueCapture";
+
+            if (!captureValue)
+            {
+                return async
+                    ? source.AnyAsync(async x => predicate(x))
+                    : source.AnyAsync(predicate);
+            }
+            else
+            {
+                return async
+                    ? source.AnyAsync(valueCapture, async (cv, x) =>
+                    {
+                        Assert.AreEqual(valueCapture, cv);
+                        return predicate(x);
+                    })
+                    : source.AnyAsync(valueCapture, (cv, x) =>
+                    {
+                        Assert.AreEqual(valueCapture, cv);
+                        return predicate(x);
+                    });
+            }
+        }
+    }
+
+    public class AnyAsyncTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            TestHelper.Setup();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            TestHelper.Cleanup();
+        }
+
+#if PROMISE_DEBUG
+        [Test]
+        public void AnyAsync_Null()
+        {
+            var enumerable = AsyncEnumerable.Range(0, 10);
+
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.AnyAsync(default(Func<int, bool>)));
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.AnyAsync("captured", default(Func<string, int, bool>)));
+                                                             
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.ConfigureAwait(SynchronizationOption.Foreground).AnyAsync(default(Func<int, bool>)));
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.ConfigureAwait(SynchronizationOption.Foreground).AnyAsync("captured", default(Func<string, int, bool>)));
+
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.AnyAsync(default(Func<int, Promise<bool>>)));
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.AnyAsync("captured", default(Func<string, int, Promise<bool>>)));
+                                                             
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.ConfigureAwait(SynchronizationOption.Foreground).AnyAsync(default(Func<int, Promise<bool>>)));
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.ConfigureAwait(SynchronizationOption.Foreground).AnyAsync("captured", default(Func<string, int, Promise<bool>>)));
+
+            enumerable.GetAsyncEnumerator().DisposeAsync().Forget();
+        }
+#endif
+
+        [Test]
+        public void AnyAsync_Empty(
+            [Values] bool configured,
+            [Values] bool async,
+            [Values] bool captureValue)
+        {
+            Promise.Run(async () =>
+            {
+                var res = new int[0].ToAsyncEnumerable()
+                    .AnyAsync(configured, async, x => x % 2 == 0, captureValue);
+                Assert.False(await res);
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void AnyAsync_Simple_True(
+            [Values] bool configured,
+            [Values] bool async,
+            [Values] bool captureValue)
+        {
+            Promise.Run(async () =>
+            {
+                var res = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable()
+                    .AnyAsync(configured, async, x => x % 2 == 0, captureValue);
+                Assert.True(await res);
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void AnyAsync_Simple_False(
+            [Values] bool configured,
+            [Values] bool async,
+            [Values] bool captureValue)
+        {
+            Promise.Run(async () =>
+            {
+                var res = new[] { 2, 8, 4 }.ToAsyncEnumerable()
+                    .AnyAsync(configured, async, x => x % 2 != 0, captureValue);
+                Assert.False(await res);
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void AnyAsync_Throw_Source(
+            [Values] bool configured,
+            [Values] bool async,
+            [Values] bool captureValue)
+        {
+            Promise.Run(async () =>
+            {
+                var ex = new Exception("Bang!");
+                var res = AsyncEnumerable<int>.Rejected(ex)
+                    .AnyAsync(configured, async, x => x % 2 == 0, captureValue);
+                await TestHelper.AssertThrowsAsync(() => res, ex);
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void AnyAsync_Throw_Predicate(
+            [Values] bool configured,
+            [Values] bool async,
+            [Values] bool captureValue)
+        {
+            Promise.Run(async () =>
+            {
+                var ex = new Exception("Bang!");
+                var res = new[] { 2, 8, 4 }.ToAsyncEnumerable()
+                    .AnyAsync(configured, async, x => { throw ex; }, captureValue);
+                await TestHelper.AssertThrowsAsync(() => res, ex);
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void AnyAsync_NoPredicate_NonEmpty()
+        {
+            Promise.Run(async () =>
+            {
+                var res = new[] { 1, 2, 3, 4 }.ToAsyncEnumerable().AnyAsync();
+                Assert.True(await res);
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void AnyAsync_NoPredicate_Empty()
+        {
+            Promise.Run(async () =>
+            {
+                var res = new int[0].ToAsyncEnumerable().AnyAsync();
+                Assert.False(await res);
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+    }
+}
+
+#endif
