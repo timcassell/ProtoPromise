60a7d259ec63cd1a8658f8b457d35f5b6be46e8e
Tim <cassell.timothy@gmail.com>
2024-01-20 06:16:20 -0500

    Added `AsyncEnumerable.ContainsAsync` extensions.

---
+++ Package/Core/Linq/Operators/ContainsAsync.cs
@@ -0,0 +1,112 @@
+#if PROTO_PROMISE_DEBUG_ENABLE || (!PROTO_PROMISE_DEBUG_DISABLE && DEBUG)
+#define PROMISE_DEBUG
+#else
+#undef PROMISE_DEBUG
+#endif
+
+using Proto.Promises.Async.CompilerServices;
+using System.Collections.Generic;
+
+namespace Proto.Promises.Linq
+{
+#if CSHARP_7_3_OR_NEWER
+    partial class AsyncEnumerable
+    {
+        /// <summary>
+        /// Determines whether an async-enumerable sequence contains a specified element by using the default equality comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The sequence in which to locate a value.</param>
+        /// <param name="value">The value to locate in the sequence.</param>
+        /// <returns>A <see cref="Promise{T}"/> resulting in whether the source sequence contains an element that has the specified value.</returns>
+        public static Promise<bool> ContainsAsync<TSource>(this AsyncEnumerable<TSource> source, TSource value)
+            => ContainsCore(source.GetAsyncEnumerator(), value, EqualityComparer<TSource>.Default);
+
+        /// <summary>
+        /// Determines whether an async-enumerable sequence contains a specified element by using a specified equality comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TEqualityComparer">The type of the elements of <paramref name="equalityComparer"/>.</typeparam>
+        /// <param name="source">The sequence in which to locate a value.</param>
+        /// <param name="value">The value to locate in the sequence.</param>
+        /// <param name="equalityComparer">An equality comparer to compare values.</param>
+        /// <returns>A <see cref="Promise{T}"/> resulting in whether the source sequence contains an element that has the specified value.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="equalityComparer"/> is null.</exception>
+        public static Promise<bool> ContainsAsync<TSource, TEqualityComparer>(this AsyncEnumerable<TSource> source, TSource value, TEqualityComparer equalityComparer)
+            where TEqualityComparer : IEqualityComparer<TSource>
+        {
+            ValidateArgument(equalityComparer, nameof(equalityComparer), 1);
+
+            return ContainsCore(source.GetAsyncEnumerator(), value, equalityComparer);
+        }
+
+        private static async Promise<bool> ContainsCore<TSource, TEqualityComparer>(this AsyncEnumerator<TSource> asyncEnumerator, TSource value, TEqualityComparer equalityComparer)
+            where TEqualityComparer : IEqualityComparer<TSource>
+        {
+            try
+            {
+                while (await asyncEnumerator.MoveNextAsync())
+                {
+                    if (equalityComparer.Equals(asyncEnumerator.Current, value))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            finally
+            {
+                await asyncEnumerator.DisposeAsync();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether a configured async-enumerable sequence contains a specified element by using the default equality comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <param name="source">The sequence in which to locate a value.</param>
+        /// <param name="value">The value to locate in the sequence.</param>
+        /// <returns>A <see cref="Promise{T}"/> resulting in whether the source sequence contains an element that has the specified value.</returns>
+        public static Promise<bool> ContainsAsync<TSource>(this ConfiguredAsyncEnumerable<TSource> source, TSource value)
+            => ContainsCore(source.GetAsyncEnumerator(), value, EqualityComparer<TSource>.Default);
+
+        /// <summary>
+        /// Determines whether a configured async-enumerable sequence contains a specified element by using a specified equality comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TEqualityComparer">The type of the elements of <paramref name="equalityComparer"/>.</typeparam>
+        /// <param name="source">The sequence in which to locate a value.</param>
+        /// <param name="value">The value to locate in the sequence.</param>
+        /// <param name="equalityComparer">An equality comparer to compare values.</param>
+        /// <returns>A <see cref="Promise{T}"/> resulting in whether the source sequence contains an element that has the specified value.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="equalityComparer"/> is null.</exception>
+        public static Promise<bool> ContainsAsync<TSource, TEqualityComparer>(this ConfiguredAsyncEnumerable<TSource> source, TSource value, TEqualityComparer equalityComparer)
+            where TEqualityComparer : IEqualityComparer<TSource>
+        {
+            ValidateArgument(equalityComparer, nameof(equalityComparer), 1);
+
+            return ContainsCore(source.GetAsyncEnumerator(), value, equalityComparer);
+        }
+
+        private static async Promise<bool> ContainsCore<TSource, TEqualityComparer>(this ConfiguredAsyncEnumerable<TSource>.Enumerator asyncEnumerator, TSource value, TEqualityComparer equalityComparer)
+            where TEqualityComparer : IEqualityComparer<TSource>
+        {
+            try
+            {
+                while (await asyncEnumerator.MoveNextAsync())
+                {
+                    if (equalityComparer.Equals(asyncEnumerator.Current, value))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            finally
+            {
+                await asyncEnumerator.DisposeAsync();
+            }
+        }
+    }
+#endif // CSHARP_7_3_OR_NEWER
+}
---
+++ Package/Tests/CoreTests/APIs/Linq/Operators/ContainsAsyncTests.cs
@@ -0,0 +1,123 @@
+﻿#if CSHARP_7_3_OR_NEWER
+
+#if PROTO_PROMISE_DEBUG_ENABLE || (!PROTO_PROMISE_DEBUG_DISABLE && DEBUG)
+#define PROMISE_DEBUG
+#else
+#undef PROMISE_DEBUG
+#endif
+
+using NUnit.Framework;
+using Proto.Promises;
+using Proto.Promises.Async.CompilerServices;
+using Proto.Promises.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace ProtoPromiseTests.APIs.Linq
+{
+    public class ContainsAsyncTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            TestHelper.Setup();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            TestHelper.Cleanup();
+        }
+
+#if PROMISE_DEBUG
+        [Test]
+        public void ContainsAsync_Null()
+        {
+            var enumerable = AsyncEnumerable.Range(0, 10);
+
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.ContainsAsync(42, default(IEqualityComparer<int>)));
+            Assert.Catch<System.ArgumentNullException>(() => enumerable.ConfigureAwait(SynchronizationOption.Synchronous).ContainsAsync(42, default(IEqualityComparer<int>)));
+
+            enumerable.GetAsyncEnumerator().DisposeAsync().Forget();
+        }
+#endif
+
+        [Test]
+        public void ContainsAsync_Simple_True(
+            [Values] bool configured)
+        {
+            Promise.Run(async () =>
+            {
+                var xs = new[] { 1, 2, 3, 4, 5 }.ToAsyncEnumerable();
+                if (configured)
+                    Assert.True(await xs.ContainsAsync(3));
+                else
+                    Assert.True(await xs.ConfigureAwait(SynchronizationOption.Foreground).ContainsAsync(3));
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void ContainsAsync_Simple_False(
+            [Values] bool configured)
+        {
+            Promise.Run(async () =>
+            {
+                var xs = new[] { 1, 2, 3, 4, 5 }.ToAsyncEnumerable();
+                if (configured)
+                    Assert.False(await xs.ContainsAsync(6));
+                else
+                    Assert.False(await xs.ConfigureAwait(SynchronizationOption.Foreground).ContainsAsync(6));
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void ContainsAsync_Simple_Comparer_True(
+            [Values] bool configured)
+        {
+            Promise.Run(async () =>
+            {
+                var xs = new[] { 1, 2, 3, 4, 5 }.ToAsyncEnumerable();
+                if (configured)
+                    Assert.True(await xs.ContainsAsync(-3, new Eq()));
+                else
+                    Assert.True(await xs.ConfigureAwait(SynchronizationOption.Foreground).ContainsAsync(-3, new Eq()));
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void ContainsAsync_Simple_Comparer_False(
+            [Values] bool configured)
+        {
+            Promise.Run(async () =>
+            {
+                var xs = new[] { 1, 2, 3, 4, 5 }.ToAsyncEnumerable();
+                if (configured)
+                    Assert.False(await xs.ContainsAsync(-6, new Eq()));
+                else
+                    Assert.False(await xs.ConfigureAwait(SynchronizationOption.Foreground).ContainsAsync(-6, new Eq()));
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        private sealed class Eq : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+            {
+                return EqualityComparer<int>.Default.Equals(Math.Abs(x), Math.Abs(y));
+            }
+
+            public int GetHashCode(int obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
+
+#endif
