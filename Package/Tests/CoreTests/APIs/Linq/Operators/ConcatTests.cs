46c38f08b1d4e5dea5b7110966d8134ba9f7d4cc
Tim <cassell.timothy@gmail.com>
2024-01-25 13:30:11 -0500

    Added a concat test.

--- Package/Tests/CoreTests/APIs/Linq/Operators/ConcatTests.cs
+++ Package/Tests/CoreTests/APIs/Linq/Operators/ConcatTests.cs
@@ -328,6 +328,42 @@ namespace ProtoPromiseTests.APIs.Linq
             }, SynchronizationOption.Synchronous)
                 .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
         }
+
+        [Test]
+        public void Concat_ConcatedBoth2_3_1()
+        {
+            Promise.Run(async () =>
+            {
+                var asyncEnumerator = new[] { 1, 2 }.ToAsyncEnumerable()
+                    .Concat(new[] { 3 }.ToAsyncEnumerable())
+                    .Concat(
+                        new[] { 4, 5 }.ToAsyncEnumerable()
+                        .Concat(new[] { 6 }.ToAsyncEnumerable())
+                        .Concat(new[] { 7 }.ToAsyncEnumerable())
+                    )
+                    .Concat(new[] { 8 }.ToAsyncEnumerable())
+                    .GetAsyncEnumerator();
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(1, asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(2, asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(3, asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(4, asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(5, asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(6, asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(7, asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual(8, asyncEnumerator.Current);
+                Assert.False(await asyncEnumerator.MoveNextAsync());
+                await asyncEnumerator.DisposeAsync();
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
     }
 }
 
