1d24172ae08b3d869c4ac15142e6ce40b48690be
Tim <cassell.timothy@gmail.com>
2024-02-09 19:39:21 -0500

    Added `AsyncEnumerable.Index` extension.

---
+++ Package/Tests/CoreTests/APIs/Linq/Operators/IndexTests.cs
@@ -0,0 +1,122 @@
+﻿#if CSHARP_7_3_OR_NEWER
+
+#if PROTO_PROMISE_DEBUG_ENABLE || (!PROTO_PROMISE_DEBUG_DISABLE && DEBUG)
+#define PROMISE_DEBUG
+#else
+#undef PROMISE_DEBUG
+#endif
+
+using NUnit.Framework;
+using Proto.Promises;
+using Proto.Promises.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace ProtoPromiseTests.APIs.Linq
+{
+    public class IndexTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            TestHelper.Setup();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            TestHelper.Cleanup();
+        }
+
+        [Test]
+        public void Index_ExpectedTuple()
+        {
+            Promise.Run(async () =>
+            {
+                var asyncEnumerator = AsyncEnumerable<string>.Create(async (writer, cancelationToken) =>
+                {
+                    await writer.YieldAsync("a");
+                    await writer.YieldAsync("b");
+                })
+                    .Index()
+                    .GetAsyncEnumerator();
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual((0, "a"), asyncEnumerator.Current);
+                Assert.True(await asyncEnumerator.MoveNextAsync());
+                Assert.AreEqual((1, "b"), asyncEnumerator.Current);
+                Assert.False(await asyncEnumerator.MoveNextAsync());
+                await asyncEnumerator.DisposeAsync();
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void Index_ExpectedIndex()
+        {
+            Promise.Run(async () =>
+            {
+                var asyncEnumerator = AsyncEnumerable.Range(0, 100)
+                    .Index()
+                    .GetAsyncEnumerator();
+                for (int index = 0; index < 100; ++index)
+                {
+                    Assert.True(await asyncEnumerator.MoveNextAsync());
+                    Assert.AreEqual(index, asyncEnumerator.Current.Index);
+                }
+                Assert.False(await asyncEnumerator.MoveNextAsync());
+                await asyncEnumerator.DisposeAsync();
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void Index_SourceThrows()
+        {
+            Promise.Run(async () =>
+            {
+                var ex = new Exception("Bang!");
+                var asyncEnumerator = AsyncEnumerable<string>.Rejected(ex)
+                    .Index()
+                    .GetAsyncEnumerator();
+                await TestHelper.AssertThrowsAsync(() => asyncEnumerator.MoveNextAsync(), ex);
+                await asyncEnumerator.DisposeAsync();
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+
+        [Test]
+        public void Index_Cancel()
+        {
+            Promise.Run(async () =>
+            {
+                var xs = AsyncEnumerable.Create<int>(async (writer, cancelationToken) =>
+                {
+                    for (int i = 0; i < 10; ++i)
+                    {
+                        cancelationToken.ThrowIfCancelationRequested();
+                        await writer.YieldAsync(i);
+                    }
+                });
+                using (var cancelationSource = CancelationSource.New())
+                {
+                    var asyncEnumerator = xs
+                        .Index()
+                        .GetAsyncEnumerator(cancelationSource.Token);
+                    Assert.True(await asyncEnumerator.MoveNextAsync());
+                    Assert.AreEqual((0, 0), asyncEnumerator.Current);
+                    Assert.True(await asyncEnumerator.MoveNextAsync());
+                    Assert.AreEqual((1, 1), asyncEnumerator.Current);
+                    cancelationSource.Cancel();
+                    await TestHelper.AssertCanceledAsync(() => asyncEnumerator.MoveNextAsync());
+                    await asyncEnumerator.DisposeAsync();
+                }
+            }, SynchronizationOption.Synchronous)
+                .WaitWithTimeoutWhileExecutingForegroundContext(TimeSpan.FromSeconds(1));
+        }
+    }
+}
+
+#endif
