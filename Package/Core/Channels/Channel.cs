c0d34bb0d0dc016c05ab865914e4335ea65cc323
Tim <cassell.timothy@gmail.com>
2024-08-04 13:47:52 -0400

    Initial Channel API design. #384

---
+++ Package/Core/Channels/Channel.cs
@@ -0,0 +1,110 @@
+﻿#if PROTO_PROMISE_DEBUG_ENABLE || (!PROTO_PROMISE_DEBUG_DISABLE && DEBUG)
+#define PROMISE_DEBUG
+#else
+#undef PROMISE_DEBUG
+#endif
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable IDE0090 // Use 'new(...)'
+
+namespace Proto.Promises.Channels
+{
+    /// <summary>
+    /// Provides a channel that supports reading and writing elements of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of data that is channeled.</typeparam>
+#if !PROTO_PROMISE_DEVELOPER_MODE
+    [DebuggerNonUserCode, StackTraceHidden]
+#endif
+    public readonly struct Channel<T> : IEquatable<Channel<T>>
+    {
+        /// <summary>
+        /// Creates a bounded channel subject to the provided options, usable by any number of readers and writers concurrently, initialized with 1 reader and 1 writer.
+        /// </summary>
+        /// <returns>The created channel.</returns>
+        public static Channel<T> NewBounded(BoundedChannelOptions<T> options)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates an unbounded channel usable by any number of readers and writers concurrently, initialized with 1 reader and 1 writer.
+        /// </summary>
+        /// <returns>The created channel.</returns>
+        public static Channel<T> NewUnbounded()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the readable half of this channel.
+        /// </summary>
+        public ChannelReader<T> Reader
+        {
+            [MethodImpl(Internal.InlineOption)]
+            get => new ChannelReader<T>(this);
+        }
+
+        /// <summary>
+        /// Gets the writable half of this channel.
+        /// </summary>
+        public ChannelWriter<T> Writer
+        {
+            [MethodImpl(Internal.InlineOption)]
+            get => new ChannelWriter<T>(this);
+        }
+
+        /// <summary>
+        /// Gets the current number of items available from this channel.
+        /// </summary>
+        public int Count
+        {
+            get => throw new NotImplementedException();
+        }
+
+        /// <summary>Returns a value indicating whether this value is equal to a specified <see cref="Channel{T}"/>.</summary>
+        [MethodImpl(Internal.InlineOption)]
+        public bool Equals(Channel<T> other)
+            => throw new NotImplementedException();
+
+        /// <summary>Returns a value indicating whether this value is equal to a specified <see cref="object"/>.</summary>
+        public override bool Equals(object obj)
+            => obj is Channel<T> other && Equals(other);
+
+        /// <summary>Returns the hash code for this instance.</summary>
+        [MethodImpl(Internal.InlineOption)]
+        public override int GetHashCode()
+            => throw new NotImplementedException();
+
+        /// <summary>Returns a value indicating whether two <see cref="Channel{T}"/> values are equal.</summary>
+        [MethodImpl(Internal.InlineOption)]
+        public static bool operator ==(Channel<T> lhs, Channel<T> rhs)
+            => throw new NotImplementedException();
+
+        /// <summary>Returns a value indicating whether two <see cref="Channel{T}"/> values are not equal.</summary>
+        [MethodImpl(Internal.InlineOption)]
+        public static bool operator !=(Channel<T> lhs, Channel<T> rhs)
+            => throw new NotImplementedException();
+
+        /// <summary>
+        /// Implicit cast from a <see cref="Channel{T}"/> to its readable half.
+        /// </summary>
+        /// <param name="channel">The <see cref="Channel{T}"/> being cast.</param>
+        /// <returns>The readable half.</returns>
+        [MethodImpl(Internal.InlineOption)]
+        public static implicit operator ChannelReader<T>(Channel<T> channel)
+            => channel.Reader;
+
+        /// <summary>
+        /// Implicit cast from a <see cref="Channel{T}"/> to its writable half.
+        /// </summary>
+        /// <param name="channel">The <see cref="Channel{T}"/> being cast.</param>
+        /// <returns>The writable half.</returns>
+        [MethodImpl(Internal.InlineOption)]
+        public static implicit operator ChannelWriter<T>(Channel<T> channel)
+            => channel.Writer;
+    }
+}
---
+++ Package/Core/Channels/ChannelWriteResult.cs
@@ -0,0 +1,79 @@
+﻿#if PROTO_PROMISE_DEBUG_ENABLE || (!PROTO_PROMISE_DEBUG_DISABLE && DEBUG)
+#define PROMISE_DEBUG
+#else
+#undef PROMISE_DEBUG
+#endif
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Proto.Promises.Channels
+{
+    /// <summary>
+    /// The result of the channel write operation.
+    /// </summary>
+    public enum ChannelWriteResult : byte
+    {
+        /// <summary>
+        /// The item was not written to the channel because all readers were disposed.
+        /// </summary>
+        Aborted,
+        /// <summary>
+        /// The item was written to the channel without removing any other item.
+        /// </summary>
+        Success,
+        /// <summary>
+        /// The channel was full and the newest item was removed to make room for the item that was written.
+        /// </summary>
+        DroppedNewest,
+        /// <summary>
+        /// The channel was full and the oldest item was removed to make room for the item that was written.
+        /// </summary>
+        DroppedOldest,
+        /// <summary>
+        /// The item was not written to the channel because the channel was full.
+        /// </summary>
+        DroppedWrite
+    }
+
+    /// <summary>
+    /// The result of writing an item to the channel.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of data that is channeled.</typeparam>
+#if !PROTO_PROMISE_DEVELOPER_MODE
+    [DebuggerNonUserCode, StackTraceHidden]
+#endif
+    public readonly struct ChannelWriteResult<T>
+    {
+        private readonly T _droppedItem;
+        private readonly ChannelWriteResult _writeResult;
+
+        /// <summary>
+        /// The result of the write operation.
+        /// </summary>
+        public ChannelWriteResult WriteResult
+        {
+            [MethodImpl(Internal.InlineOption)]
+            get => _writeResult;
+        }
+
+        [MethodImpl(Internal.InlineOption)]
+        internal ChannelWriteResult(ChannelWriteResult writeResult, T droppedItem)
+        {
+            _writeResult = writeResult;
+            _droppedItem = droppedItem;
+        }
+
+        /// <summary>
+        /// Get the item that was dropped if an item was dropped. An item was dropped if <see cref="WriteResult"/> != <see cref="ChannelWriteResult.Success"/>.
+        /// </summary>
+        /// <param name="droppedItem">When this method returns, contains the dropped item if an item was dropped, <see langword="default"/> otherwise.</param>
+        /// <returns><see langword="true"/> if an item was dropped, <see langword="false"/> otherwise.</returns>
+        [MethodImpl(Internal.InlineOption)]
+        public bool TryGetDroppedItem(out T droppedItem)
+        {
+            droppedItem = _droppedItem;
+            return _writeResult != ChannelWriteResult.Success;
+        }
+    }
+}
