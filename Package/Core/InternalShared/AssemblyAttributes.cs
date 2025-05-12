efb62c9894bb25ccaf2825ef62dc5a4a5e388293
Tim Cassell <cassell.timothy@gmail.com>
2025-05-11 22:49:07 -0400

    Fix old Unity IL2CPP tests.

--- Package/Core/InternalShared/AssemblyAttributes.cs
+++ Package/Core/InternalShared/AssemblyAttributes.cs
@@ -7,6 +7,8 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ProtoPromise.UnityHelpers.2018.3")]
 [assembly: InternalsVisibleTo("ProtoPromise.UnityHelpers.2023.1")]
+// Old Unity IL2CPP fails tests with TestCaseSource if the assembly name has a . in it, so we remove the . from the test assembly name in Unity.
+[assembly: InternalsVisibleTo("ProtoPromiseTests")]
 [assembly: InternalsVisibleTo("ProtoPromise.Tests")]
 [assembly: InternalsVisibleTo("ProtoPromise.Tests.UnityHelpers.2018.3")]
 [assembly: InternalsVisibleTo("ProtoPromise.Tests.UnityHelpers.2023.1")]
