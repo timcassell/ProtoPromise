8554d881660b7899d4ffb3cd5aa7401caf4bcc6b
Tim Cassell <cassell.timothy@gmail.com>
2025-08-01 01:02:49 -0400

    Fixed compile errors.

--- Package/Core/ForOldRuntime/HashCode.cs
+++ Package/Core/ForOldRuntime/HashCode.cs
@@ -44,6 +44,7 @@ https://raw.githubusercontent.com/Cyan4973/xxHash/5c174cfa4e45a42f94082dc0d4539b
 
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 #pragma warning disable CA1066 // Implement IEquatable when overriding Object.Equals
