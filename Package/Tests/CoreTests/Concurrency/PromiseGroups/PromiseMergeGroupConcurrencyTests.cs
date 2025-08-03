8e51674f875c20a099301876fbc5913c426789cf
Tim Cassell <cassell.timothy@gmail.com>
2025-08-03 05:49:13 -0400

    Added promise group cleanup concurrency tests. Fixed race condition.

--- Package/Tests/CoreTests/Concurrency/PromiseGroups/PromiseMergeGroupConcurrencyTests.cs
+++ Package/Tests/CoreTests/Concurrency/PromiseGroups/PromiseMergeGroupConcurrencyTests.cs
@@ -10,6 +10,7 @@ using NUnit.Framework;
 using Proto.Promises;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace ProtoPromise.Tests.Concurrency.PromiseGroups
 {
@@ -32,6 +33,7 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
         [Test]
         public void DeferredsMayBeCompletedWhileTheirPromisesArePassedToPromiseMergeGroup_AndCancelationTriggeredConcurrently_T_void(
             [Values] bool withCancelation,
+            [Values] bool withCleanup,
             [Values] CombineType combineType,
             [Values] CompleteType completeType1,
             [Values] bool alreadyComplete1,
@@ -61,12 +63,26 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
                 parallelActions.Add(() => cancelationSource.Cancel());
             }
 
+            int resolveCount = 0;
+            int cleanupCount = 0;
+            long completedFlag = 0;
+            Action<int> onCleanup = _ =>
+            {
+                Assert.AreEqual(0, Interlocked.Read(ref completedFlag));
+                Interlocked.Increment(ref cleanupCount);
+            };
+
             var helper = ParallelCombineTestHelper.Create(
                 combineType,
-                () => group
-                    .Add(promise1)
-                    .Add(promise2)
-                    .WaitAsync(),
+                withCleanup
+                    ? () => group
+                        .Add(promise1, onCleanup)
+                        .Add(promise2)
+                        .WaitAsync()
+                    : () => group
+                        .Add(promise1)
+                        .Add(promise2)
+                        .WaitAsync(),
                 expectedResolveValue: 1
             );
             helper.MaybeAddParallelAction(parallelActions);
@@ -87,6 +103,13 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
                     }
                     promise1 = TestHelper.BuildPromise(completeType1, alreadyComplete1, 1, rejectValue, groupCancelationToken, out tryCompleter1);
                     promise2 = TestHelper.BuildPromise(completeType2, alreadyComplete2, rejectValue, groupCancelationToken, out tryCompleter2);
+                    completedFlag = 0;
+                    if (withCleanup)
+                    {
+                        resolveCount = 0;
+                        cleanupCount = 0;
+                        promise1 = promise1.Then(v => { Interlocked.Increment(ref resolveCount); return v; });
+                    }
                     helper.Setup();
                 },
                 // teardown
@@ -98,6 +121,13 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
                         cancelationSource.Dispose();
                     }
                     Assert.IsTrue(helper.Success);
+                    if (withCleanup)
+                    {
+                        if (helper.State != Promise.State.Resolved)
+                            Assert.AreEqual(resolveCount, cleanupCount);
+                        else
+                            Assert.AreEqual(0, cleanupCount);
+                    }
                 },
                 parallelActions
             );
@@ -106,6 +136,7 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
         [Test]
         public void DeferredsMayBeCompletedWhileTheirPromisesArePassedToPromiseMergeGroup_AndCancelationTriggeredConcurrently_void_T_void_T(
             [Values] bool withCancelation,
+            [Values] bool withCleanup,
             [Values] CombineType combineType,
             [Values] CompleteType completeType1,
             [Values] bool alreadyComplete1,
@@ -152,14 +183,30 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
                 parallelActions.Add(() => cancelationSource.Cancel());
             }
 
+            int resolveCount = 0;
+            int cleanupCount = 0;
+            long completedFlag = 0;
+            Action<int> onCleanup = _ =>
+            {
+                Assert.AreEqual(0, Interlocked.Read(ref completedFlag));
+                Interlocked.Increment(ref cleanupCount);
+            };
+
             var helper = ParallelCombineTestHelper.Create(
                 combineType,
-                () => group
-                    .Add(promise1)
-                    .Add(promise2)
-                    .Add(promise3)
-                    .Add(promise4)
-                    .WaitAsync(),
+                withCleanup
+                    ? () => group
+                        .Add(promise1)
+                        .Add(promise2, onCleanup)
+                        .Add(promise3)
+                        .Add(promise4, onCleanup)
+                        .WaitAsync()
+                    : () => group
+                        .Add(promise1)
+                        .Add(promise2)
+                        .Add(promise3)
+                        .Add(promise4)
+                        .WaitAsync(),
                 expectedResolveValue: (1, 2)
             );
             helper.MaybeAddParallelAction(parallelActions);
@@ -182,6 +229,14 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
                     promise2 = TestHelper.BuildPromise(completeType2, alreadyComplete2, 1, rejectValue, groupCancelationToken, out tryCompleter2);
                     promise3 = TestHelper.BuildPromise(completeType3, alreadyComplete3, rejectValue, groupCancelationToken, out tryCompleter3);
                     promise4 = TestHelper.BuildPromise(completeType4, alreadyComplete4, 2, rejectValue, groupCancelationToken, out tryCompleter4);
+                    completedFlag = 0;
+                    if (withCleanup)
+                    {
+                        resolveCount = 0;
+                        cleanupCount = 0;
+                        promise2 = promise2.Then(v => { Interlocked.Increment(ref resolveCount); return v; });
+                        promise4 = promise4.Then(v => { Interlocked.Increment(ref resolveCount); return v; });
+                    }
                     helper.Setup();
                 },
                 // teardown
@@ -193,6 +248,13 @@ namespace ProtoPromise.Tests.Concurrency.PromiseGroups
                         cancelationSource.Dispose();
                     }
                     Assert.IsTrue(helper.Success);
+                    if (withCleanup)
+                    {
+                        if (helper.State != Promise.State.Resolved)
+                            Assert.AreEqual(resolveCount, cleanupCount);
+                        else
+                            Assert.AreEqual(0, cleanupCount);
+                    }
                 },
                 parallelActions
             );
